11441c1611639fd4e9003a5efc2854354f83af18
Storm Dragon <stormdragon2976@gmail.com>
2018-10-19 19:43:25 -0400

    random now dials a 10 digit number.

--- espeaker-phone.sh
+++ espeaker-phone.sh
@@ -3,7 +3,7 @@
 if [[ $# -gt 0 ]]; then
     dial=($(echo "$@" | tr -cd '[:digit:]' | sed 's/\([0-9]\)/\1 /g'))
 else
-    dial=($(shuf -n7 -e 0 1 2 3 4 5 6 7 8 9))
+    dial=($(shuf -n10 -e 0 1 2 3 4 5 6 7 8 9))
 fi
 
 number=(schschschtktktktktktktktktktk schtk schtktk schtktktk schschtktktktk schschtktktktktk schschtktktktktktk schschschtktktktktktktk schschschtktktktktktktktk schschschtktktktktktktktktk)
